78329d86f3e4bb58e62e4dfa5a16565be5dd5845
Timothy <lemayiant4@gmail.com>
2022-08-23 11:22:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    const textToDelete = document.getElementById('text');
+    const theText = textToDelete.textContent
+    textToDelete.textContent= "This is really cool!"
+});
+
